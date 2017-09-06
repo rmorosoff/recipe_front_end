d30de30853fec478628348adba4aac2655827aba
MikeJohnstone <michael.johnstone@libertymutual.com>
2017-09-06 15:53:49 -0700

    my changes

--- api/controllers/RecipeController.js
+++ api/controllers/RecipeController.js
@@ -7,35 +7,37 @@
 
 var Client = require('node-rest-client').Client;
 var client = new Client();
-var endpoint = "http://localhost:1337/recipe";
+var endpoint = "https://pure-waters-78967.herokuapp.com/api/recipes";
 
 module.exports = {
 
   /**
    * `recipesController.create()`
    */
-  create: function (req, res) {
+  create: function(req, res) {
 
-        if(req.method != "POST"){
-          return res.view('create');
-        }
+    if (req.method != "POST") {
+      return res.view('create');
+    }
 
-        var args = {
-            data: req.body,
-            headers: { "Content-Type": "application/json" }
-        };
+    var args = {
+      data: req.body,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
 
-        client.post(endpoint, args, function (data, response) {
-            // return res.view('create', {success: { message: "Record added successfully"}});
-            if(response.statusCode != "201"){
-                req.addFlash("error", data.message.substring(data.message.indexOf("•")));
-                return res.redirect('/create');
-            }
+    client.post(endpoint, args, function(data, response) {
+      // return res.view('create', {success: { message: "Record added successfully"}});
+      if (response.statusCode != "201") {
+        req.addFlash("error", data.message.substring(data.message.indexOf("•")));
+        return res.redirect('/create');
+      }
 
-            req.addFlash("success", "Record created successfully");
-            return res.redirect('/create');
+      req.addFlash("success", "Record created successfully");
+      return res.redirect('/create');
 
-        })
+    })
 
   },
 
@@ -43,42 +45,56 @@ module.exports = {
   /**
    * `recipesController.read()`
    */
-  read: function (req, res) {
+  read: function(req, res) {
 
-    client.get(endpoint, function (data, response) {
-        return res.view('read', {recipes: data});
-    }).on('error', function (err) {
-        return res.view('read', {error: { message: "There was an error getting the recipes"}});
+    client.get(endpoint, function(data, response) {
+      return res.view('read', {
+        recipes: data
+      });
+    }).on('error', function(err) {
+      return res.view('read', {
+        error: {
+          message: "There was an error getting the recipes"
+        }
+      });
     });
 
   },
 
 
-   /**
+  /**
    * `recipesController.update()`
    */
-  update: function (req, res) {
+  update: function(req, res) {
 
-    if(req.method != "POST"){
+    if (req.method != "POST") {
 
-      client.get(endpoint, function (data, response) {
-        return res.view('update', {recipes: data});
-      }).on('error', function (err) {
-          return res.view('update', {error: { message: "There was an error getting the recipes"}});
+      client.get(endpoint, function(data, response) {
+        return res.view('update', {
+          recipes: data
+        });
+      }).on('error', function(err) {
+        return res.view('update', {
+          error: {
+            message: "There was an error getting the recipes"
+          }
+        });
       });
 
-    }else{
+    } else {
 
       var args = {
-          data: req.body,
-          headers: { "Content-Type": "application/json" }
+        data: req.body,
+        headers: {
+          "Content-Type": "application/json"
+        }
       };
 
-      client.put(endpoint + "/" + req.body.id, args, function (data, response) {
+      client.put(endpoint + "/" + req.body.id, args, function(data, response) {
 
-        if(response.statusCode != "200"){
-            req.addFlash("error", data.message);
-            return res.redirect('/update');
+        if (response.statusCode != "200") {
+          req.addFlash("error", data.message);
+          return res.redirect('/update');
         }
 
         req.addFlash("success", "Record updated successfully");
@@ -92,23 +108,29 @@ module.exports = {
   /**
    * `recipesController.delete()`
    */
-  delete: function (req, res) {
+  delete: function(req, res) {
 
-    if(req.method != "POST"){
+    if (req.method != "POST") {
 
-      client.get(endpoint, function (data, response) {
-        return res.view('delete', {recipes: data});
-      }).on('error', function (err) {
-          return res.view('delete', {error: { message: "There was an error getting the recipes"}});
+      client.get(endpoint, function(data, response) {
+        return res.view('delete', {
+          recipes: data
+        });
+      }).on('error', function(err) {
+        return res.view('delete', {
+          error: {
+            message: "There was an error getting the recipes"
+          }
+        });
       });
 
-    }else{
+    } else {
 
-      client.delete(endpoint + "/" + req.body.recipe_id, function (data, response) {
+      client.delete(endpoint + "/" + req.body.recipe_id, function(data, response) {
 
-        if(response.statusCode != "200"){
-            req.addFlash("error", data.message);
-            return res.redirect('/delete');
+        if (response.statusCode != "200") {
+          req.addFlash("error", data.message);
+          return res.redirect('/delete');
         }
 
         req.addFlash("success", "Record deleted successfully");
